66a8ce216b43903e8fffcb330a6bdb96add88bb9
Mohammed Mahamuud M <moha714566@gmail.com>
2025-05-28 12:20:13 +0200

    Map to test the geolocation API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 // here we'll soon manipulate the UI 
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HelloPage from './components/HelloPage/HelloPage';
+import MapPage from './components/MapPage/MapPage';
 
 const App: React.FC = () => {
-    return (
-        <div>
-            <HelloPage />
-        </div>
-    );
-};
+  return (
+    <Router>
+      <Routes>
+        {/* Home page */}
+        <Route path="/" element={<HelloPage />} />
 
+        {/* Maps page */}
+        <Route path="/maps" element={<MapPage />} />
+      </Routes>
+    </Router>
+  );
+};
 export default App;
